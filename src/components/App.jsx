16c89ed6ce8a129b0ecad593f6e2ebf48771ca29
Lina Zoo <linazooper@gmail.com>
2022-04-20 13:46:53 -0500

    finished building the UI for the home component and set up a route to render a new component TeamPage when the user visits the url that matches the url parameter for the team id

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 import Teams from "./Teams";
 import Players from "./Players";
+import TeamPage from "./TeamPage";
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/players" element={<Players />} />
           <Route path="/teams" element={<Teams />} />
+          <Route path="/:teamId" element={<TeamPage />} />
         </Routes>
       </div>
     </Router>
---
+++ src/components/TeamPage.jsx
@@ -0,0 +1,6 @@
+import { useParams } from "react-router-dom";
+
+export default function TeamPage() {
+  const { teamId } = useParams();
+  return <div>Team page for {teamId}</div>;
+}
