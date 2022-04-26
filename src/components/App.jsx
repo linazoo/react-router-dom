8a3909f4d830cf2f80f282bf070b00b264dc81f8
Lina Zoo <linazooper@gmail.com>
2022-04-26 12:40:31 -0500

    built out UI for the teams route so I can select a team and render the UI for the teams using nested routes and url parameters

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Navbar";
 import Home from "./Home";
 import Teams from "./Teams";
 import Players from "./Players";
 import TeamPage from "./TeamPage";
 import Player from "./Player";
+import Team from "./Team";
 
 export default function App() {
   return (
@@ -15,9 +16,11 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/players" element={<Players />}>
-            <Route path=":playerId" element={<Player />}></Route>
+            <Route path=":playerId" element={<Player />} />
           </Route>
-          <Route path="/teams" element={<Teams />} />
+          <Route path="/teams" element={<Teams />}>
+            <Route path=":teamId" element={<Team />} />
+          </Route>
           <Route path="/:teamId" element={<TeamPage />} />
         </Routes>
       </div>
--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -1,3 +1,19 @@
+import useTeamNames from "../hooks/useTeamNames";
+import Sidebar from "./Sidebar";
+import { Outlet } from "react-router-dom";
+
 export default function Teams() {
-  return <div className="container">Teams</div>;
+  const { response: teamNames, loading } = useTeamNames();
+
+  if (loading === true) {
+    return null;
+  }
+
+  return (
+    <div className="container two-column">
+      <Sidebar title="Teams" list={teamNames} />
+
+      <Outlet />
+    </div>
+  );
 }
